0db9c70277ae79ba5ec9ff83a39834c14c7a22f7
toric <157854112+Torusdeev@users.noreply.github.com>
2025-10-02 22:07:46 -0700

    good text box

--- Jammmm/Assets/Scripts/for textbox/TextInput.cs
+++ Jammmm/Assets/Scripts/for textbox/TextInput.cs
@@ -1,4 +1,5 @@
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -33,14 +34,43 @@ public class TextInput : MonoBehaviour
             {
                 inputAction.RespondToInput(controller, separatedInputWords);
             }
+            else
+            {
+                if (userInput != "/")
+                {
+                    controller.actionLog.Add("...type '/' for help." + "\n");
+                }
+                
+            }
             InputComplete();
-        } //OLD
+        } //OLD ORIGINAL LOOP!
+
+        //for (int i = 0; i < userInput.Length; i++)
+        //{
+
+        //    if (userInput.Contains(Exit.keyString))
+        //    {
+        //        inputAction.RespondToInput(controller, separatedInputWords);
+        //    }
+        //    else
+        //    {
+        //        print("there is no keyword in this statement");
+        //    }
+        //    InputComplete();
+        //} //NEW LOOP does not work lol
+
+
+
 
 
         //TEXT THAT CAN BE INTERACTED WITH AT ANY POINT OF THE GAME...
         if (userInput == "the original") 
         {
             controller.actionLog.Add("starwalker?" + "\n");
+        }
+        if (userInput == "/")
+        {
+            controller.actionLog.Add("'/' for help..." + "\n" + "'go to the _' to move..." + "\n" + "and many more..." + "\n");
         }
         InputComplete();
     }
